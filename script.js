ffa67a30dd89ef7b7bef4b21721cd30d461d37a8
Mahia <mtq1386@gmail.com>
2024-11-17 07:05:44 +0330

    ,

---
+++ script.js
@@ -0,0 +1,16 @@
+
+let a = +prompt('a?', '');
+
+switch (a) {
+    case a === 0:
+        alert(0);
+        break;
+    case a === 1:
+        alert(1);
+        break;
+    case a == 2 || 3:
+        alert('2,3');
+        break;
+    default:
+        break;
+}
